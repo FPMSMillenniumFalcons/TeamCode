45001e53012f9ac530d8ad7ae45948735680098e
FPMSMillenniumFalcons <42720837+FPMSMillenniumFalcons@users.noreply.github.com>
2018-10-14 20:55:28 -0500

    High level flow.

--- src/main/java/org/firstinspires/ftc/teamcode/MFAuto.java
+++ src/main/java/org/firstinspires/ftc/teamcode/MFAuto.java
@@ -20,57 +20,40 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 @Autonomous(name = "MFAuto")
 public class MFAuto extends LinearOpMode {
-    BNO055IMU imu;
+    /*
+     * instantiate chassis object. chassis object should contains all driver capability.
+     * should have following methods:
+     *    chassis.shift_left(double distance or double time);
+     *    chassis.shift_right(double distance or double time);
+     *    chassis.turn_left(double degress);
+     *    chassis.turn_right(double degress);
+     *    chassis.go_straight(double distance or double time); + foward, - backward
+     * optional:
+     *    chassis.go_leftdiag(double distance or double time); + foward, - backward
+     *    chassis.go_rightdiag(double distance or double time); + foward, - backward
+     */
+    //HardwarePushbotChassis chassis = new HardwarePushbotChassis(); //FIXME:
 
-    // State used for updating telemetry
-    Orientation angles;
-    Acceleration gravity;
+    /*
+     * instantiate lift and claw object and should have following method
+     *     lift_claw.lift_self(double height) // extent, hook, retract
+     *     lift_claw.lower_self(double height) // extend, unhook, retract
+     *     lift_claw.close()
+     *     lift_claw.open()
+     *     lift_claw.updown(double height) + up, <=0 down and reset
+     *     lift_claw.extend(double length) + extend out, <=0 retract
+     */
+    //HardWarePushbotLiftClaw lift_claw = new HardWarePushbotLiftClaw(); //FIXME:
 
-    /*DcMotor leftDrive;
-    DcMotor leftDriveB;
-    DcMotor rightDrive;
-    DcMotor rightDriveB;*/
+    /*
+     * instantiate ColorDistanceSensor. this could be part of chassis object
+     *     colorDistant.is_color_yellow()
+     *     colorDistant.query_distant()
+     */
+    //HardWarePushbotColorDistant colorDistant = new HardWarePushbotColorDistant(); //FIXME:
 
-    HardwarePushbot2 robot = new HardwarePushbot2();   // Use a Pushbot's hardware
+    //HardwarePushbot2 robot = new HardwarePushbot2();   // Use a Pushbot's hardware
     private ElapsedTime runtime = new ElapsedTime();
-
-    ColorSensor sensorColor1;
-    ColorSensor sensorColor2;
-    DistanceSensor sensorDistance1;
-    DistanceSensor sensorDistance2;
-
-
-    private float[] is_color_yellow(ColorSensor sensorColor) {
-        // hsvValues is an array that will hold the hue, saturation, and value information.
-        float hsvValues[] = {0F, 0F, 0F};
-
-        // values is a reference to the hsvValues array.
-        final float values[] = hsvValues;
-
-        // sometimes it helps to multiply the raw RGB values with a scale factor
-        // to amplify/attentuate the measured values.
-        final double SCALE_FACTOR = 255;
-        // convert the RGB values to HSV values.
-        // multiply by the SCALE_FACTOR.
-        // then cast it back to int (SCALE_FACTOR is a double)
-        Color.RGBToHSV((int) (sensorColor.red() * SCALE_FACTOR),
-                (int) (sensorColor.green() * SCALE_FACTOR),
-                (int) (sensorColor.blue() * SCALE_FACTOR),
-                hsvValues);
-
-        telemetry.addData("IN:Distance (cm) 1",
-                String.format(Locale.US, "%.02f", sensorDistance.getDistance(DistanceUnit.CM)));
-        telemetry.addData("IN: Alpha1", sensorColor.alpha());
-        telemetry.addData("IN: Red1  ", sensorColor.red());
-        telemetry.addData("IN: Green1", sensorColor.green());
-        telemetry.addData("IN: Blue1 ", sensorColor.blue());
-
-        telemetry.addData("IN: getDeviceName ", sensorColor.getDeviceName());
-        telemetry.addData("IN: Hue1", hsvValues[0]);
-        telemetry.update();
-
-        return hsvValues;
-    }
 
     //@Override
     public void runOpMode() throws InterruptedException {
@@ -79,47 +62,21 @@ public class MFAuto extends LinearOpMode {
          * Initialize the drive system variables.
          * The init() method of the hardware class does all the work here
          */
-        robot.init(hardwareMap);
-        float hsvValues1[], hsvValues2[];
-
-        sensorColor1 = hardwareMap.get(ColorSensor.class, "sensor_color_distance1");
-        sensorColor2 = hardwareMap.get(ColorSensor.class, "sensor_color_distance2");
-
-        // get a reference to the distance sensor that shares the same name.
-        sensorDistance1 = hardwareMap.get(DistanceSensor.class, "sensor_color_distance1");
-        sensorDistance2 = hardwareMap.get(DistanceSensor.class, "sensor_color_distance2");
-        // Send telemetry message to signify robot waiting;
+        //chassis(hardwareMap);  //FIXME:
+        //lift_claw(hardwareMap);  //FIXME:
+        //colorDistant(hardwarMap);  //FIXME:
 
         /*
          * main loop of autonomous
          */
         while (opModeIsActive()) {
-            // hsvValues is an array that will hold the hue, saturation, and value information.
-            hsvValues1 = is_color_yellow(sensorColor1);
-            hsvValues2 = is_color_yellow(sensorColor2);
+            // lift_claw.lift_self()  //FIXME:
 
-            /* send the info back to driver station using telemetry function.
-            telemetry.addData("Distance (cm) 1",
-                    String.format(Locale.US, "%.02f", sensorDistance1.getDistance(DistanceUnit.CM)));
-            telemetry.addData("Alpha1", sensorColor1.alpha());
-            telemetry.addData("Red1  ", sensorColor1.red());
-            telemetry.addData("Green1", sensorColor1.green());
-            telemetry.addData("Blue1 ", sensorColor1.blue());
+            // lift_claw.lower_self()  //FIXME:
 
-            telemetry.addData("getDeviceName ", sensorColor1.getDeviceName());
-            telemetry.addData("Hue1", hsvValues1[0]);
-
-            telemetry.addData("Distance (cm)2",
-                    String.format(Locale.US, "%.02f", sensorDistance2.getDistance(DistanceUnit.CM)));
-            telemetry.addData("Alpha2", sensorColor2.alpha());
-            telemetry.addData("Red2  ", sensorColor2.red());
-            telemetry.addData("Green2", sensorColor2.green());
-            telemetry.addData("Blue2 ", sensorColor2.blue());
-            telemetry.addData("getDeviceName ", sensorColor2.getDeviceName());
-            telemetry.addData("Hue2", hsvValues2[0]);
-            */
-            telemetry.update();
-
+            //FIXME:
+            // chassis drive to next station
+            // etc the whole game should be here.
         }
 
     }
